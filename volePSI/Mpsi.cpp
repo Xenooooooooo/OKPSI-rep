0eb57a5880bae224958203708d1b433ccee5d4b5
lx1234 <lx1234@buaa.edu.cn>
2023-12-02 01:52:37 +0800

    protocol v2.0

--- volePSI/Mpsi.cpp
+++ volePSI/Mpsi.cpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <map>
 #include <future>
+#include <thread>
 #include "volePSI/SimpleIndex.h"
 #include "libdivide.h"
 #include "coproto/Socket/AsioSocket.h"
@@ -27,7 +28,7 @@ namespace volePSI
                 Chl[i]=coproto::asioConnect("localhost:" + std::to_string(BASE_PORT + i), true);
 
             setTimePoint("Chl Finish");
-            //std::cout <<"Chl Finish"<<std::endl;
+            // std::cout <<"Reciver: Chl Finish"<<std::endl;
 
             std::vector<block> Rand_Num(Set_Size);
             Prng.get<block>(Rand_Num);
@@ -35,26 +36,40 @@ namespace volePSI
             Paxos.init(Set_Size, GCT_Bin_Size, CUCKOO_HASH_NUM, Lambda, PaxosParam::GF128, Seed);
             u64 Paxos_Size = Paxos.size();
             std::vector<block> GCT(Paxos_Size);
-            Paxos.solve<block>(Inputs, Rand_Num, GCT, &Prng, Thread_Num);
+            
+            // std::cout <<"Receiver: before encode"<<std::endl;
+            
+            // if prng is nullptr and numThreads > 1, it will be wrong
+            Paxos.solve<block>(Inputs, Rand_Num, GCT, &Prng, 1);
 
             setTimePoint("GCT Finish");
-            //std::cout <<"GCT Finish"<<std::endl;
+            // std::cout <<"Receiver: GCT Finish" << std::endl;
 
-            std::vector<block> Share(Paxos_Size);
+            std::vector<std::vector<block>> Share(User_Num - 2);
 
             for (u64 i = 0ull; i < User_Num - 1 - 1; i++){
-                Prng.get<block>(Share);
-                coproto::sync_wait(Chl[i].send(Share));
+                Share[i].resize(Paxos_Size);
+                Prng.get<block>(Share[i]);
                 for (u64 j = 0ull; j < Paxos_Size; j++)
-                    GCT[j] = GCT[j] ^ Share[j];
+                    GCT[j] = GCT[j] ^ Share[i][j];
             }
-            coproto::sync_wait(Chl[User_Num - 1 - 1].send(GCT));
+
+            std::vector<std::thread> shareThrds(User_Num - 1);
+            for (u64 i = 0; i < User_Num - 2; ++i){
+                shareThrds[i] = std::thread([&, i]() {
+                    coproto::sync_wait(Chl[i].send(Share[i]));
+                });
+            }
+            shareThrds[User_Num - 2] = std::thread([&, User_Num]() {
+                coproto::sync_wait(Chl[User_Num - 2].send(GCT));
+            });
+            for (auto& thrd : shareThrds) thrd.join();
 
             setTimePoint("Share Finish");
-            //std::cout <<"Share Finish"<<std::endl;
+            // std::cout <<"Reciver: Share Finish"<<std::endl;
 
 
-            coproto::sync_wait(Chl[User_Num -1 -1].flush());
+            // coproto::sync_wait(Chl[User_Num -1 -1].flush());
             
             setTimePoint("2PSI Begin");
 
@@ -66,88 +81,129 @@ namespace volePSI
             Psi_Receiver.init(Set_Size,Set_Size,Lambda,Seed,false,Thread_Num);
             auto p = Psi_Receiver.run(Rand_Num,Chl[User_Num -1 -1]);
             auto re = macoro::sync_wait(macoro::when_all_ready(std::move(p)));
-            coproto::sync_wait(Chl[User_Num -1 -1].flush());
 
             setTimePoint("2PSI Finish");
-            //std::cout <<"2-PSI Finish"<<std::endl;           
+            // std::cout <<"Reciver: 2-PSI Finish"<<std::endl;           
 
             Size_Intersection = Psi_Receiver.mIntersection.size();
             Multi_Intersection.clear();
             for (u64 i = 0ull; i < Size_Intersection; i++)
                 Multi_Intersection.push_back(Inputs[Psi_Receiver.mIntersection[i]]);
-
-            for (u64 i = 0ull; i < User_Num - 1; i++)
-                Comm += Chl[i].bytesSent();
-
-                
             
             setTimePoint("Get Intersection Finish");
+
+            for (u64 i = 0ull; i < User_Num - 1; i++){
+                coproto::sync_wait(Chl[i].flush());
+                Comm += Chl[i].bytesSent();
+                Chl[i].close();
+            }
             //std::cout <<"Get Intersection Finish"<<std::endl;  
             //std::cout << User <<std::endl;
-
-            for (u64 i = 0ull; i < User_Num - 1; i++)
-                Chl[i].close();
+            setTimePoint("Close Chl Finish");
+            // std::cout <<"Reciver: Close Chl Finish"<<std::endl;
         }
-        else{
-            Chl.resize(3);
-            Chl[0]=coproto::asioConnect("localhost:" + std::to_string(BASE_PORT + My_Id), false);
-            if (My_Id != 0ull) Chl[1]=coproto::asioConnect("localhost:" + std::to_string(BASE_PORT + 100 + My_Id), true);
-            if (My_Id != User_Num -1 -1) Chl[2]=coproto::asioConnect("localhost:" + std::to_string(BASE_PORT + 100 + My_Id + 1), false);
+        else if (My_Id == User_Num - 2)
+        {
+            Chl.resize(User_Num - 1);
+            Chl[0] = coproto::asioConnect("localhost:" + std::to_string(BASE_PORT + My_Id), false);
+            for (u64 i = 1; i < User_Num - 1; i++)
+                Chl[i] = coproto::asioConnect("localhost:" + std::to_string(BASE_PORT + My_Id * 100 + i - 1), false);
 
             setTimePoint("Chl Finish");
-
-            std::vector<block> Share,Docode_Share(Set_Size);
-            coproto::sync_wait(Chl[0].recvResize(Share));
-
-            setTimePoint("Share Finish");
+            // std::cout <<"Sender: Chl Finish"<<std::endl;
 
             Baxos Paxos;
             Paxos.init(Set_Size, GCT_Bin_Size, CUCKOO_HASH_NUM, Lambda, PaxosParam::GF128, Seed);
-            u64 Paxos_Size = Paxos.size();
-            std::vector<block> GCT(Paxos_Size);
-            Paxos.decode<block>(Inputs, Docode_Share, Share, Thread_Num);
-            Paxos.solve<block>(Inputs, Docode_Share, GCT, &Prng, Thread_Num);
-            if (My_Id != 0ull){
-                std::vector<block> GCT_Receive;
-                coproto::sync_wait(Chl[1].recvResize(GCT_Receive));
-                for (u64 i = 0ull; i < Paxos_Size; i++)
-                    GCT[i]= GCT[i] ^ GCT_Receive[i];
+            
+            std::vector<std::vector<block>> GCT(User_Num - 1);
+            GCT[0].resize(Paxos.size());
+            coproto::sync_wait(Chl[0].recv(GCT[0]));
+            std::vector<block> Result(Set_Size);
+            Paxos.decode<block>(Inputs, Result, GCT[0], Thread_Num);
+
+            // recv GCT from P1 - Pn-2
+            std::vector<std::thread> recvThrds(User_Num - 2);
+            for (u64 i = 1; i < User_Num - 1; ++i){
+                recvThrds[i - 1] = std::thread([&, i]() {
+                    GCT[i].resize(Paxos.size());
+                    coproto::sync_wait(Chl[i].recv(GCT[i]));
+                });
             }
+            for (auto& thrd : recvThrds) thrd.join();
+
+            setTimePoint("receive GCT Finish");
+            // std::cout <<"Sender: receive GCT Finish"<<std::endl;
+            
+            for (u64 i = 1; i < User_Num - 1; i++){
+                for (u64 j = 0; j < Paxos.size(); j++)
+                    GCT[0][j] ^= GCT[i][j];
+            }
+            // std::cout << "Sender: GCT oplus finish" << std::endl;
+            Paxos.decode<block>(Inputs, Result, GCT[0], Thread_Num);
+
+            setTimePoint("Decode Finish");
+            // std::cout <<"Sender: Decode Finish"<<std::endl;
+
+            RsPsiSender Psi_Sender;
+            // if(mTimer)
+            //     Psi_Sender.setTimer(getTimer());
+
+            Psi_Sender.init(Set_Size,Set_Size,Lambda,Seed,false,Thread_Num);
+            auto p = Psi_Sender.run(Result,Chl[0]);
+            auto re = macoro::sync_wait(macoro::when_all_ready(std::move(p)));
+
+            setTimePoint("2PSI Finish");
+            // std::cout <<"Sender: 2-PSI Finish"<<std::endl;
+
+            for (u64 i = 0ull; i < User_Num - 1; i++){
+                coproto::sync_wait(Chl[i].flush());
+                Comm += Chl[i].bytesSent();
+                Chl[i].close();
+            }
+
+            setTimePoint("Close Chl Finish");
+            // std::cout <<"Sender: Close Chl Finish"<<std::endl;
+        }
+        else
+        {
+            Chl.resize(2);
+            Chl[0] = coproto::asioConnect("localhost:" + std::to_string(BASE_PORT + My_Id), false);
+            Chl[1] = coproto::asioConnect("localhost:" + std::to_string(BASE_PORT + (User_Num - 2) * 100 + My_Id), true);
+
+            setTimePoint("Chl Finish");
+            // std::cout <<"Client: Chl Finish" << std::endl;
+
+            Baxos Paxos;
+            Paxos.init(Set_Size, GCT_Bin_Size, CUCKOO_HASH_NUM, Lambda, PaxosParam::GF128, Seed);
+            
+            std::vector<block> Share(Paxos.size()), Decode_Share(Set_Size);
+            coproto::sync_wait(Chl[0].recv(Share));
+
+            setTimePoint("receive Share Finish");
+            // std::cout <<"Client: receive Share Finish" << std::endl;
+
+            std::vector<block> GCT(Paxos.size());
+            Paxos.decode<block>(Inputs, Decode_Share, Share, Thread_Num);
+
+            // if prng is nullptr and numThreads > 1, it will be wrong
+            Paxos.solve<block>(Inputs, Decode_Share, GCT, &Prng, 1);
 
             setTimePoint("GCT Reconstruction Finish");
+            // std::cout <<"Client: GCT Reconstruction Finish" << std::endl;
 
-            if (My_Id != User_Num -1 -1){
-                coproto::sync_wait(Chl[2].send(GCT));
-                setTimePoint("Send GCT Finish");
-            }
-            else{
-                
-                std::vector<block> Result(Set_Size);
-                Paxos.decode<block>(Inputs, Result, GCT, Thread_Num);
-
-                setTimePoint("Final Decoding Finish");
-
-                coproto::sync_wait(Chl[0].flush());
-
-                setTimePoint("2PSI Begin");                
-
-                RsPsiSender Psi_Sender;
-                // if(mTimer)
-                //     Psi_Sender.setTimer(getTimer());
-
-                Psi_Sender.init(Set_Size,Set_Size,Lambda,Seed,false,Thread_Num);
-                auto p = Psi_Sender.run(Result,Chl[0]);
-                auto re = macoro::sync_wait(macoro::when_all_ready(std::move(p)));
-
-                setTimePoint("2PSI Finish");
-
-                coproto::sync_wait(Chl[0].flush());
-                
-            }
-            Comm += Chl[(My_Id == User_Num -1 -1)?(0):(2)].bytesSent();
+            coproto::sync_wait(Chl[1].send(GCT));
+            
+            setTimePoint("Send GCT Finish");
+            // std::cout <<"Client: Send GCT Finish" << std::endl;
+            
+            coproto::sync_wait(Chl[0].flush());
+            coproto::sync_wait(Chl[1].flush());
+            Comm += Chl[1].bytesSent();
             Chl[0].close();
-            if (My_Id != 0ull) Chl[1].close();
-            if (My_Id != User_Num -1 -1) Chl[2].close();
+            Chl[1].close();
+
+            setTimePoint("Close Chl Finish");
+            // std::cout <<"Client: Close Chl Finish" << std::endl;
         }
         return ;
     }
